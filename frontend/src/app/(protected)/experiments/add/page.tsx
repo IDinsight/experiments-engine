b65109952f9ba773bca138504551a1de5ee80c96
Sid Ravinutala <sid.ravi1@gmail.com>
2025-02-20 00:57:05 -0500

    updated prev/next/submit

--- frontend/src/app/(protected)/experiments/add/page.tsx
+++ frontend/src/app/(protected)/experiments/add/page.tsx
@@ -4,6 +4,11 @@ import { AllSteps } from "./addExperimentSteps";
 import { ExperimentProvider } from "./components/AddExperimentContext";
 import AddBasicInfo from "./components/basicInfo";
 import { Button } from "@/components/catalyst/button";
+import {
+  PlusIcon,
+  ChevronRightIcon,
+  ChevronLeftIcon,
+} from "@heroicons/react/20/solid";
 
 export default function NewExperiment() {
   const [currentStep, setCurrentStep] = useState(0);
@@ -33,15 +38,24 @@ export default function NewExperiment() {
       </div>
       <div className="flex justify-between max-w-4xl mx-auto mt-8">
         <Button onClick={prevStep} disabled={currentStep === 0}>
+          <ChevronLeftIcon className="h-5 w-5" />
           Previous
         </Button>
-        <Button
-          className="px-4 py-2 bg-gray-200 rounded"
-          onClick={nextStep}
-          disabled={currentStep === steps.length}
-        >
-          Next
-        </Button>
+        {currentStep === steps.length ? (
+          <button
+            type="button"
+            className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            onClick={() => console.log("Create Experiment")}
+          >
+            <PlusIcon aria-hidden="true" className="-ml-0.5 mr-1.5 h-5 w-5" />
+            Create Experiment
+          </button>
+        ) : (
+          <Button className="px-4 py-2 bg-gray-200 rounded" onClick={nextStep}>
+            Next
+            <ChevronRightIcon className="h-5 w-5" />
+          </Button>
+        )}
       </div>
     </ExperimentProvider>
   );
