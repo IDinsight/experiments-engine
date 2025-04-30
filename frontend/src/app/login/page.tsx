efeaf82b4a46b7139624bed23365d7756a12ce7a
Sid Ravinutala <sid.ravi1@gmail.com>
2025-04-30 16:17:07 -0400

    minor aesthetic fixes

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -70,7 +70,7 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-purple-50 to-blue-100 dark:from-indigo-950 dark:to-blue-950 p-4">
+    <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-purple-50 to-blue-100 dark:from-black dark:to-blue-950 p-4">
       <motion.div
         initial={{ opacity: 0, y: -20 }}
         animate={{ opacity: 1, y: 0 }}
