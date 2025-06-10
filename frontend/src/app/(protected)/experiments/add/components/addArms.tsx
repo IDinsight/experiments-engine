2365592947b0bd557d006db16777783310c17e73
poornimaramesh <poornimaramesh1995@gmail.com>
2025-06-10 10:25:45 +0300

    add input arms page

---
+++ frontend/src/app/(protected)/experiments/add/components/addArms.tsx
@@ -0,0 +1,388 @@
+import {
+  useExperimentStore,
+  isMABExperimentStateBeta,
+  isBayesianABState
+} from "../../store/useExperimentStore";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@/components/ui/label";
+import type {
+  NewArm,
+  StepComponentProps,
+} from "../../types";
+import { Plus, Trash } from "lucide-react";
+import { DividerWithTitle } from "@/components/Dividers";
+import { useCallback, useEffect, useState, useMemo } from "react";
+
+export default function AddMABArms({ onValidate }: StepComponentProps) {
+  const { experimentState, updateArm, addArm, removeArm } =
+    useExperimentStore();
+
+  const [inputValues, setInputValues] = useState<Record<string, string>>({});
+
+  const bayesABarms: Record<number, string> = {1: "Treatment Arm", 2: "Control Arm"};
+
+  const baseArmDesc = useMemo(
+    () => ({
+      name: "",
+      description: "",
+    }),
+    []
+  );
+
+  const additionalArmErrors = useMemo(
+    () =>
+      experimentState.prior_type === "beta"
+        ? { alpha_init: "", beta_init: "" }
+        : { mu_init: "", sigma_init: "" },
+    [experimentState]
+  );
+
+  const [errors, setErrors] = useState(() => {
+    return experimentState.arms.map(() => {
+      return { ...baseArmDesc, ...additionalArmErrors };
+    });
+  });
+
+  const validateForm = useCallback(() => {
+    let isValid = true;
+    const newErrors = experimentState.arms.map(() => ({
+      ...baseArmDesc,
+      ...additionalArmErrors,
+    }));
+
+    experimentState.arms.forEach((arm, index) => {
+      if (!arm.name.trim()) {
+        newErrors[index].name = "Arm name is required";
+        isValid = false;
+      }
+
+      if (!arm.description.trim()) {
+        newErrors[index].description = "Description is required";
+        isValid = false;
+      }
+
+      if (experimentState.prior_type === "beta") {
+        if ("alpha_init" in arm) {
+          if (!arm.alpha_init) {
+            newErrors[index].alpha_init = "Alpha prior is required";
+            isValid = false;
+          }
+          if (arm.alpha_init && arm.alpha_init <= 0) {
+            newErrors[index].alpha_init =
+              "Alpha prior should be greater than 0";
+            isValid = false;
+          }
+        }
+
+        if ("beta_init" in arm) {
+          if (!arm.beta_init) {
+            newErrors[index].beta_init = "Beta prior is required";
+            isValid = false;
+          }
+
+          if (arm.beta_init && arm.beta_init <= 0) {
+            newErrors[index].beta_init = "Beta prior should be greater than 0";
+            isValid = false;
+          }
+        }
+      } else if (experimentState.prior_type === "normal") {
+        if ("mu_init" in arm && typeof arm.mu_init !== "number") {
+          newErrors[index].mu_init = "Mean value is required";
+          isValid = false;
+        }
+
+        if ("sigma_init" in arm) {
+          if (!arm.sigma_init) {
+            newErrors[index].sigma_init = "Std. deviation is required";
+            isValid = false;
+          }
+
+          if (arm.sigma_init && arm.sigma_init <= 0) {
+            newErrors[index].sigma_init =
+              "Std deviation should be greater than 0";
+            isValid = false;
+          }
+        }
+      }
+    });
+    return { isValid, newErrors };
+  }, [experimentState, baseArmDesc, additionalArmErrors]);
+
+  useEffect(() => {
+    const { isValid, newErrors } = validateForm();
+    if (JSON.stringify(newErrors) !== JSON.stringify(errors)) {
+      setErrors(newErrors);
+      onValidate({
+        isValid,
+        errors: newErrors.map((error) =>
+          Object.fromEntries(
+            Object.entries(error).map(([key, value]) => [key, value ?? ""])
+          )
+        ),
+      });
+    }
+  }, [validateForm, onValidate, errors]);
+
+  useEffect(() => {
+    const newInputValues: Record<string, string> = {};
+
+    if (!isMABExperimentStateBeta(experimentState)) {
+      experimentState.arms.forEach((arm, index) => {
+        newInputValues[`${index}-mu`] = (
+          (arm as NewArm).mu_init || 0
+        ).toString();
+      });
+    }
+    setInputValues(newInputValues);
+  }, [experimentState]);
+
+  const handleNumericChange = (index: number, value: string) => {
+    // Update the local input state for a smooth typing experience
+    setInputValues((prev) => ({
+      ...prev,
+      [`${index}-mu`]: value,
+    }));
+
+    if (value !== "" && value !== "-") {
+      const numValue = Number.parseFloat(value);
+      if (!isNaN(numValue)) {
+        updateArm(index, { mu_init: numValue });
+      }
+    }
+  };
+
+  return (
+    <div>
+      <div className="flex w-full flex-wrap items-end justify-between gap-4 border-b border-zinc-950/10 pb-6 dark:border-white/10">
+        <h2 className="text-2xl font-semibold tracking-tight">Add Experiment Arms</h2>
+        <div className="flex gap-4">
+          <Button className="mt-4" onClick={addArm} disabled={isBayesianABState(experimentState)}>
+            <Plus className="w-4 h-4 mr-2" />
+            Add Arm
+          </Button>
+          <Button
+            className="mt-4 mx-4"
+            disabled={experimentState.arms.length <= 2 || isBayesianABState(experimentState)}
+            variant="outline"
+            onClick={() => {
+              removeArm(experimentState.arms.length - 1);
+            }}
+          >
+            <Trash className="w-4 h-4 mr-2" />
+            Delete Arm
+          </Button>
+        </div>
+      </div>
+      <div className="space-y-6" aria-label="Add Experiment Arms">
+        {experimentState.arms.map((arm, index) => (
+          <div key={index}>
+            <DividerWithTitle title={isBayesianABState(experimentState) ? bayesABarms[index + 1] : `Arm ${index + 1}`} />
+            <div className="md:flex md:flex-row md:space-x-8 md:space-y-0 items-start">
+              <div className="basis-1/2">
+                <div className="flex flex-col mb-4">
+                  <div className="flex flex-row items-start">
+                    <Label
+                      className="basis-1/4 mt-2 font-medium"
+                      htmlFor={`arm-${index + 1}-name`}
+                    >
+                      Name
+                    </Label>
+                    <div className="basis-3/4 flex flex-col">
+                      <Input
+                        id={`arm-${index + 1}-name`}
+                        name={`arm-${index + 1}-name`}
+                        placeholder="Give the arm a searchable name"
+                        value={arm.name || ""}
+                        onChange={(e) =>
+                          updateArm(index, { name: e.target.value })
+                        }
+                      />
+                      {errors[index]?.name ? (
+                        <p className="text-destructive text-xs mt-1">
+                          {errors[index].name}
+                        </p>
+                      ) : (
+                        <p className="text-destructive text-xs mt-1">&nbsp;</p>
+                      )}
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-col">
+                  <div className="flex flex-row items-start">
+                    <Label
+                      className="basis-1/4 mt-2 font-medium"
+                      htmlFor={`arm-${index + 1}-description`}
+                    >
+                      Description
+                    </Label>
+                    <div className="basis-3/4 flex flex-col">
+                      <Textarea
+                        id={`arm-${index + 1}-description`}
+                        name={`arm-${index + 1}-description`}
+                        placeholder="Describe the arm"
+                        value={arm.description || ""}
+                        onChange={(e) =>
+                          updateArm(index, { description: e.target.value })
+                        }
+                      />
+                      {errors[index]?.description ? (
+                        <p className="text-destructive text-xs mt-1">
+                          {errors[index].description}
+                        </p>
+                      ) : (
+                        <p className="text-destructive text-xs mt-1">&nbsp;</p>
+                      )}
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {experimentState.prior_type === "beta" && (
+                <div className="basis-1/2 grow">
+                  <div className="flex flex-col mb-4">
+                    <div className="flex flex-row items-start">
+                      <Label
+                        className="basis-1/4 mt-2 font-medium"
+                        htmlFor={`arm-${index + 1}-alpha`}
+                      >
+                        Alpha prior
+                      </Label>
+                      <div className="basis-3/4 flex flex-col">
+                        <Input
+                          id={`arm-${index + 1}-alpha`}
+                          name={`arm-${index + 1}-alpha`}
+                          placeholder="Enter an integer as the prior for the alpha parameter"
+                          value={(arm as NewArm).alpha_init || ""}
+                          onChange={(e) => {
+                            updateArm(index, {
+                              alpha_init: parseInt(e.target.value),
+                            });
+                          }}
+                        />
+                        {errors[index]?.alpha_init ? (
+                          <p className="text-destructive text-xs mt-1">
+                            {errors[index].alpha_init}
+                          </p>
+                        ) : (
+                          <p className="text-destructive text-xs mt-1">
+                            &nbsp;
+                          </p>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex flex-col">
+                    <div className="flex flex-row items-start">
+                      <Label
+                        className="basis-1/4 mt-2 font-medium"
+                        htmlFor={`arm-${index + 1}-beta`}
+                      >
+                        Beta prior
+                      </Label>
+                      <div className="basis-3/4 flex flex-col">
+                        <Input
+                          id={`arm-${index + 1}-beta`}
+                          name={`arm-${index + 1}-beta`}
+                          placeholder="Enter an integer as the prior for the beta parameter"
+                          value={(arm as NewArm).beta_init || ""}
+                          onChange={(e) => {
+                            updateArm(index, {
+                              beta_init: parseInt(e.target.value),
+                            });
+                          }}
+                        />
+                        {errors[index]?.beta_init ? (
+                          <p className="text-destructive text-xs mt-1">
+                            {errors[index].beta_init}
+                          </p>
+                        ) : (
+                          <p className="text-destructive text-xs mt-1">
+                            &nbsp;
+                          </p>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )}
+              {experimentState.prior_type === "normal" && (
+                <div className="basis-1/2 grow">
+                  <div className="flex flex-col mb-4">
+                    <div className="flex flex-row items-start">
+                      <Label
+                        className="basis-1/4 mt-2 font-medium"
+                        htmlFor={`arm-${index + 1}-mu`}
+                      >
+                        Mean prior
+                      </Label>
+                      <div className="basis-3/4 flex flex-col">
+                        <Input
+                          id={`arm-${index + 1}-mu`}
+                          name={`arm-${index + 1}-mu`}
+                          type="number"
+                          placeholder="Enter a float as mean for the prior"
+                          defaultValue={0}
+                          value={
+                            inputValues[`${index}-mu`] ??
+                            (arm as NewArm).mu_init?.toString()
+                          }
+                          onChange={(e) => {
+                            handleNumericChange(index, e.target.value);
+                          }}
+                        />
+                        {errors[index]?.mu_init ? (
+                          <p className="text-destructive text-xs mt-1">
+                            {errors[index].mu_init}
+                          </p>
+                        ) : (
+                          <p className="text-destructive text-xs mt-1">
+                            &nbsp;
+                          </p>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex flex-col">
+                    <div className="flex flex-row items-start">
+                      <Label
+                        className="basis-1/4 mt-2 font-medium"
+                        htmlFor={`arm-${index + 1}-sigma`}
+                      >
+                        Standard deviation
+                      </Label>
+                      <div className="basis-3/4 flex flex-col">
+                        <Input
+                          id={`arm-${index + 1}-sigma`}
+                          name={`arm-${index + 1}-sigma`}
+                          type="number"
+                          defaultValue={1}
+                          placeholder="Enter a float as standard deviation for the prior"
+                          value={(arm as NewArm).sigma_init || ""}
+                          onChange={(e) => {
+                            updateArm(index, {
+                              sigma_init: Number(e.target.value),
+                            });
+                          }}
+                        />
+                        {errors[index]?.sigma_init ? (
+                          <p className="text-destructive text-xs mt-1">
+                            {errors[index].sigma_init}
+                          </p>
+                        ) : (
+                          <p className="text-destructive text-xs mt-1">
+                            &nbsp;
+                          </p>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- frontend/src/app/(protected)/experiments/add/components/addExperimentSteps.tsx
+++ frontend/src/app/(protected)/experiments/add/components/addExperimentSteps.tsx
@@ -1,65 +1,89 @@
-import { Step } from "../../types";
-import AddMABArms from "./mabs/addMABArms";
-import MABPriorRewardSelection from "./mabs/addPriorReward";
-import AddCMABArms from "./cmabs/addCMABArms";
-import AddCMABContexts from "./cmabs/addCMABContext";
-import CMABPriorRewardSelection from "./cmabs/addPriorReward";
-import AddBayesABArms from "./bayes_ab/addBayesABArms";
-import BayesianABRewardSelection from "./bayes_ab/addPriorReward";
-import AddNotifications from "./addNotifications";
+import { MethodType, Step } from "../../types";
+import PriorRewardSelection  from "./addPriorReward";
+import AddContext from "./addContext";
+import AddArms from "./addArms";
+// import AddMABArms from "./mabs/addMABArms";
+// import MABPriorRewardSelection from "./mabs/addPriorReward";
+// import AddCMABArms from "./cmabs/addCMABArms";
+// import AddCMABContexts from "./cmabs/addCMABContext";
+// import CMABPriorRewardSelection from "./cmabs/addPriorReward";
+// import AddBayesABArms from "./bayes_ab/addBayesABArms";
+// import BayesianABRewardSelection from "./bayes_ab/addPriorReward";
+// import AddNotifications from "./addNotifications";
+
+
+const AllSteps = (exp_type: MethodType): Step[] => {
+  const steps = [{
+    name: "Configure Prior and Outcome for Experiment",
+    component: PriorRewardSelection,
+  },
+  {
+    name: "Add Arms",
+    component: AddArms,
+  },
+
+]
+  if (exp_type === "cmab") {
+    steps.splice(0, 0, {
+      name: "Configure Prior and Reward",
+      component: AddContext,
+    });
+  }
+  return steps;
+};
 
 // --- MAB types and steps ---
 
-const MABsteps: Step[] = [
-  {
-    name: "Configure MAB",
-    component: MABPriorRewardSelection,
-  },
-  {
-    name: "Add Arms",
-    component: AddMABArms,
-  },
-  { name: "Notifications", component: AddNotifications },
-];
+// const MABsteps: Step[] = [
+//   {
+//     name: "Configure MAB",
+//     component: MABPriorRewardSelection,
+//   },
+//   {
+//     name: "Add Arms",
+//     component: AddMABArms,
+//   },
+//   { name: "Notifications", component: AddNotifications },
+// ];
 
 // --- CMAB test types and steps ---
 
-const CMABsteps: Step[] = [
-  {
-    name: "Configure MAB",
-    component: CMABPriorRewardSelection,
-  },
-  {
-    name: "Add Contexts",
-    component: AddCMABContexts,
-  },
-  {
-    name: "Add Arms",
-    component: AddCMABArms,
-  },
-  { name: "Notifications", component: AddNotifications },
-];
+// const CMABsteps: Step[] = [
+//   {
+//     name: "Configure MAB",
+//     component: CMABPriorRewardSelection,
+//   },
+//   {
+//     name: "Add Contexts",
+//     component: AddCMABContexts,
+//   },
+//   {
+//     name: "Add Arms",
+//     component: AddCMABArms,
+//   },
+//   { name: "Notifications", component: AddNotifications },
+// ];
 
 // --- A/B test types and steps ---
 
-const BayesianABsteps: Step[] = [
-  {
-    name: "Configure Bayesian A/B Test",
-    component: BayesianABRewardSelection,
-  },
-  {
-    name: "Add Arms",
-    component: AddBayesABArms,
-  },
-  { name: "Notifications", component: AddNotifications },
-];
+// const BayesianABsteps: Step[] = [
+//   {
+//     name: "Configure Bayesian A/B Test",
+//     component: BayesianABRewardSelection,
+//   },
+//   {
+//     name: "Add Arms",
+//     component: AddBayesABArms,
+//   },
+//   { name: "Notifications", component: AddNotifications },
+// ];
 
 // --- All steps ---
 
-const AllSteps = {
-  mab: MABsteps,
-  cmab: CMABsteps,
-  bayes_ab: BayesianABsteps,
-};
+// const AllSteps = {
+//   mab: MABsteps,
+//   cmab: CMABsteps,
+//   bayes_ab: BayesianABsteps,
+// };
 
 export { AllSteps };
